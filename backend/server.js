154d23368450845556f869b683c5fea5210a8e0c
Parth Ajmera <ajmeraparth05@gmail.com>
2025-06-17 03:40:33 +0530

    Some more deployment fixes

--- backend/server.js
+++ backend/server.js
@@ -160,7 +160,4 @@ cron.schedule("*/8 * * * *", async () => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
